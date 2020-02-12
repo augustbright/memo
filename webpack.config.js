0fb4d4a4c950a07f812b9c0f1607173e94712980
vtumanov782 <vtumanov782@gmail.com>
2020-02-12 16:53:21 +0300

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.ts'),
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {test: /.\.ts$/, exclude: /node_modules/, use: 'awesome-typescript-loader'},
+            {test: /.\.tsx$/, exclude: /node_modules/, use: 'awesome-typescript-loader'}
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'templates', 'index.html'),
+            filename: path.join(__dirname, 'build', 'index.html'),
+            hash: true
+        })
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, 'build')
+    },
+
+    resolve: {
+        extensions: ['*', '.js', '.ts', '.tsx']
+    }
+};
